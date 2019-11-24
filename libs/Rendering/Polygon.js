c3e6ba9612d9819d557dbf2aca5da144d8db9766
Nikos <nikos@plugachev.com>
2019-11-24 14:31:29 -0500

    Added Polygon class

---
+++ libs/Rendering/Polygon.js
@@ -0,0 +1,15 @@
+
+class Polygon {
+
+  constructor() {
+
+  }
+
+  display() {
+    noFill();
+    stroke(255);
+    strokeWeight(2);
+    ellipse(width / 2, height / 2, 100, 100);
+  }
+
+}
--- scripts/setup.js
+++ scripts/setup.js
@@ -1,4 +1,7 @@
 
+
+let p = new Polygon();
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(255);
